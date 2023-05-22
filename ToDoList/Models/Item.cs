7bebbf9b947d128746e89ffd5a5a8bddef1d8f91
Laura Hope <ssteamlaura@gmail.com>
2023-05-22 11:18:35 -0700

    add files

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -9,6 +9,6 @@ namespace ToDoList.Models
     public int CategoryId { get; set; }
     public Category Category { get; set; }
     public List<ItemTag> JoinEntities { get;}
-    public bool IsComplete { get; set; }
+    // public bool IsComplete { get; set; }
   }
 }
