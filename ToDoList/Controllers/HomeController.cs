24135eb0eb2c6e0f4d8d8b70ae24fa295846e0e8
Laura Hope <ssteamlaura@gmail.com>
2023-05-23 14:56:28 -0700

    Add splash page info

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ToDoListContext _db;
+
+    public HomeController(ToDoListContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Category[] cats = _db.Categories.ToArray();
+      Item[] items = _db.Items.ToArray();
+      Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+      model.Add("categories", cats);
+      model.Add("items", items);
+      return View(model);
+    }
+  }
 }
